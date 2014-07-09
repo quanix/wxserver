ad07748d4f1880e0e1193e02676df507cf339891
quanix <quanix@163.com>
2014-07-09 15:59:07 +0800

    提交基本代码

---
+++ app/src/main/java/com/domac/app/web/controller/IndexController.java
@@ -0,0 +1,17 @@
+package com.domac.app.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * created by lihaoquan
+ */
+@Controller
+public class IndexController {
+
+    @RequestMapping("/")
+    public String index(Model model) {
+        return "index";
+    }
+}
