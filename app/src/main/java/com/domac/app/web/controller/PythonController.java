f337b279531556baf1e5b50af6fa0d222c6c7716
quanix <quanix@163.com>
2014-08-01 16:52:04 +0800

    提交接口文件

--- app/src/main/java/com/domac/app/web/controller/PythonController.java
+++ app/src/main/java/com/domac/app/web/controller/PythonController.java
@@ -38,6 +38,7 @@ public class PythonController {
 
         String sn_1 = getValue(dataMap, "sn_1", true);
 
+        //Hi
         System.out.println("sn_1===>"+sn_1);
 
         String density_name = getValue(dataMap, "density_name", false);
